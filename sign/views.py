120cda100f007a6a63ee3893cb43de913d6f2915
wixshq <1756033641@qq.com>
2018-01-05 21:10:58 +0800

    Django Demo

---
+++ sign/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+#登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == 'admin123':
+            # return HttpResponse("Login Success!")
+            return HttpResponseRedirect('/event_manage/')
+        else:
+            return render(request, 'index.html', {'error':'username or password error!'})
+#发布会管理
+def event_manage(request):
+    return render(request,'event_manage.html')
+
